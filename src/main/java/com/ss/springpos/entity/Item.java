a331100287b6935732f17c83b82a4da082a68e01
SubhashanaSS <subhashana201@gmail.com>
2024-10-14 10:54:18 +0530

    initial commit

---
+++ src/main/java/com/ss/springpos/entity/Item.java
@@ -0,0 +1,22 @@
+package com.ss.springpos.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Double price;
+
+    private String description;
+}
---
+++ src/main/java/com/ss/springpos/entity/ItemCategory.java
@@ -0,0 +1,22 @@
+package com.ss.springpos.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
+public class ItemCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "item")
+    private List<Item> items;
+}
